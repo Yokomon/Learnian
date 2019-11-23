56610ad4f4d205d5256114ee7b9376b16208532f
Yokomon <marowmars@gmail.com>
2019-11-23 23:35:12 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './Nav';
 import Section from "./Section.js";
+import Section2 from "./Section2";
 class App extends React.Component{
     render(){
         return(
             <div>
                 <Nav></Nav>
                 <Section></Section>
+                <Section2></Section2>
             </div>
         )
     }
