b18d2b6f0684fa7393cec534fac6dea8a1da3d55
Yokomon <marowmars@gmail.com>
2019-11-23 15:02:17 +0100

    Fixing it

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Learnian</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/user/signup">Sign up</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/user/login">Log-in</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
