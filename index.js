d8d82176845e5e3c2f228a235724dc097a0f4714
Philzace1 <unknownumeh@gmail.com>
2020-02-12 02:42:40 +0100

    database integration

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+// import dependenc
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
-// const morgan = require('morgan');
+const morgan = require('morgan');
+// import constant from dotenv
 const { DB, PORT } = process.env;
 const app = express();
+// using middleware for bodyParser
 app.use(express.json);
-// mongoose
-//   .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to DB');
-//   })
-//   .catch(err => console.log(err));
+// connecting to the database
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch(err => console.log(err));
+// line for deployment
 if (process.env.NODE_ENV === 'production') {
   //set a static folder
   app.use(express.static('client/build'));
@@ -20,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+app.use('/api/users', require('./routes/api/users'));
 
-// const PORT = process.env.PORT || 3000;
-
+// starting server
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT} 🔥`));
--- model/User.js
+++ model/User.js
@@ -2,15 +2,36 @@ const mongoose = require('mongoose');
 
 const schema = mongoose.Schema;
 
-const User = new schema({
-  name: {
+const UserSchema = new schema({
+  username: {
     type: String,
     required: true
   },
+  passoword: {
+    type: String,
+    required: true
+  },
+  UserId: {
+    type: String,
+    required: true
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  isLecturer: {
+    type: Boolean,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true
+  },
+
   date: {
     type: Date,
     default: Date.now
   }
 });
 
-module.exports = User = mongoose.model('user', User);
+module.exports = User = mongoose.model('user', UserSchema);
